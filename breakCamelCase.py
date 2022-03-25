02d073a2fa2ba0835344a2467bb0608c149d73df
nickync <lizheng9024@gmail.com>
2022-03-25 18:35:42 -0400

    codewars

---
+++ breakCamelCase.py
@@ -0,0 +1,27 @@
+""" 
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+ """
+
+def solution(s):
+    sl = [x for x in s]
+    ans =""
+    for i in sl:
+        if i.isupper():
+            ans += ' '
+            ans += i
+        else:
+            ans += i            
+    return ans
+
+
+ # Best Solution
+ def solution(s):
+    return ''.join(' ' + c if c.isupper() else c for c in s)
