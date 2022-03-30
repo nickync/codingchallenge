d39f12356c0d8f4a501fce8a0878597c8b8be6c0
nickync <lizheng9024@gmail.com>
2022-03-29 22:35:01 -0400

    codewars

---
+++ twoSum.py
@@ -0,0 +1,26 @@
+""" 
+
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+"""
+
+def two_sum(numbers, target):
+    
+    for i in range(len(numbers)):
+        j = i + 1
+        while j <len(numbers):
+            if numbers[i] + numbers[j] == target :
+                return [i,j]
+            j+=1
+
+# Best Solution
+
+def two_sum(nums, t):
+    for i, x in enumerate(nums):
+        for j, y in enumerate(nums):
+            if i != j and x + y == t:
+                return [i, j]
