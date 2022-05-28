1a4f88097fca2696c2972a9b5a838901181262b4
nickync <lizheng9024@gmail.com>
2022-05-27 21:18:34 -0700

    format

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -24,7 +24,7 @@ function likes(names) {
         return `${names[i]}, ${names[i+1]} and ${names.length - 2} others like this`
       }
     }
-  }
+}
 
   // Best Solution
 
@@ -37,4 +37,4 @@ function likes(names) {
       case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
       default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
     }
-  }
+}
