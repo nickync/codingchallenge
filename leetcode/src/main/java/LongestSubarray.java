b94c5d0c420e7ba3ef360513462b87d87b35c673
nickync <lizheng9024@gmail.com>
2024-01-10 23:41:05 -0500

    leetcode

--- leetcode/src/main/java/LongestSubarray.java
+++ leetcode/src/main/java/LongestSubarray.java
@@ -1,26 +1,27 @@
 public class LongestSubarray {
     public static void main(String[] args) {
-        System.out.println(longestSubarray(new int[]{0,1,1,1,0,1,1,0,1}));
+        System.out.println(longestSubarray(new int[]{1,1,1,0,1,1,1,0,1,1,1,1,1}));
+        //System.out.println(longestSubarray(new int[]{1,1,1}));
     }
 
     public static int longestSubarray(int[] nums) {
+        int left = 0;
+        int zero = 0;
         int res = 0;
-        int max = 0;
-        int del = 1;
-        for (int i =0; i <nums.length; i++){
-            if (nums[i] == 1){
-                res ++;
-            } else {
-                if (del == 1){
 
-                    del = 0;
-                } else {
-                    max = res;
-                    del = 1;
-                    res = 0;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zero++;
             }
+            while (zero > 1) {
+                if (nums[left]==0){
+                    zero --;
+                }
+                left ++;
+            }
+            res = Math.max(res, i - left);
         }
+
         return res;
     }
 }
