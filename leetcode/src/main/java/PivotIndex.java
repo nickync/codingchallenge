d66d33768713616628f630761e1841f6ebaef2d7
nickync <lizheng9024@gmail.com>
2024-01-17 22:50:11 -0500

    leet

--- leetcode/src/main/java/PivotIndex.java
+++ leetcode/src/main/java/PivotIndex.java
@@ -1,29 +1,28 @@
 public class PivotIndex {
     public static void main(String[] args) {
+        System.out.println(pivotIndex(new int[]{2,1,-1}));
         System.out.println(pivotIndex(new int[]{1,7,3,6,5,6}));
+        System.out.println(pivotIndex(new int[]{1,2,3}));
     }
 
     public static int pivotIndex(int[] nums) {
-        int left =0;
-        int right=nums.length-1;
-        int sum1=0;
-        int sum2=0;
-        int ind = -1;
-        while (right > left){
-            sum1 += nums[left];
-            sum2 += nums[right];
-            if (sum1 > sum2){
-                right ++;
-            } else if (sum1 < sum2){
-                left ++;
-            } else {
-                left ++;
-                right--;
-            }
-            if (sum1 == sum2){
-                ind = right;
-            }
+
+        int sum = 0;
+        for (int n : nums){
+            sum += n;
         }
-        return sum1 == sum2 ? right : -1;
+        int left =0;
+        int right=sum;
+
+        for (int i=0; i<nums.length; i++){
+            right -= nums[i];
+            if (left == right){
+                return i;
+            }
+            left += nums[i];
+        }
+
+        return -1;
+
     }
 }
